a2555f9ee69a03932a3cd7cb9c37d4d6e6da4808
adrian-blip <58608388+adrian-blip@users.noreply.github.com>
2021-06-30 17:04:10 -0500

    Correct

--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""user.py module"""
-from models.base_model import BaseModel
+from models import *
 
 
 class User(BaseModel):
-    """User class"""
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
