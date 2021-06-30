7d6d6e192c39f9e98cea051887b70fac76adc92c
samilo-del <camilovelenciapadilla@gmail.com>
2021-06-30 22:25:17 +0000

    add file

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.engine import file_storage
 
 
 storage = file_storage.FileStorage()
