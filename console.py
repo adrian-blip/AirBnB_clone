0c4994b2a0566cc5d65d21f1bc5e6f2998e0f83e
samilo-del <camilovelenciapadilla@gmail.com>
2021-06-30 18:46:55 +0000

    add file console

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""HBNB"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+
+    """Simple command processor example."""
+
+    prompt = '(hbnb)'
+
+    def do_EOF(self, line):
+        """EOF"""
+        return True
+
+    def do_quit(self, args):
+        """ Close cmd """
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
