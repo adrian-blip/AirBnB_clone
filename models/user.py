09af3e0d657ebbc5ac914b70ee8f20b4b9c9cb9a
JaimeGarcia24 <jaimegarcia9999c@gmail.com>
2021-07-01 03:45:06 +0000

    console

--- models/user.py
+++ models/user.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-from models import *
+"""
+class User
+"""
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    '''
+    Class User
+    '''
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
