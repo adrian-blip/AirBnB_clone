1d3c503c76fcf414b84b8367ba356bbb8ef67e9a
JaimeGarcia24 <jaimegarcia9999c@gmail.com>
2021-07-01 02:01:01 +0000

    correction

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""Import Class"""
+"""from models.base_model import BaseModel
 from models.engine import file_storage
 
 
 storage = file_storage.FileStorage()
 storage.reload()
+"""
---
+++ models/engine/.#file_storage.py
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.1846:1625101135
